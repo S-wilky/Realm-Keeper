0d7b5f9e41bdc32903c2967476acff311bc90e22
Rebekah Barts <rabarts@student.fullsail.edu>
2026-01-20 16:57:55 -0500

    Update phi3Model.py to use Hugging Face model with HF_TOKEN

--- ai-service/app/model/phi3Model.py
+++ ai-service/app/model/phi3Model.py
@@ -1,42 +1,46 @@
 import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
-from peft import PeftModel
+# from peft import PeftModel
 
-# Disable accelerate auto-dispatch
-os.environ["NO_ACCELERATE"] = "1"
-os.environ["ACCELERATE_DISABLE_MPS_FALLBACK"] = "1"
+# # Disable accelerate auto-dispatch
+# os.environ["NO_ACCELERATE"] = "1"
+# os.environ["ACCELERATE_DISABLE_MPS_FALLBACK"] = "1"
 
-BASE_MODEL = "microsoft/phi-3-mini-4k-instruct"
+# BASE_MODEL = "microsoft/phi-3-mini-4k-instruct"
 # LORA_DIR = "phi3_qlora_out"
-HF_MODEL = os.getenv("AI_SERVICE_URL")
+HF_MODEL = "Eckkeh/rk-phi3-model"
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 # Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, use_fast=True)
+tokenizer = AutoTokenizer.from_pretrained(HF_MODEL, use_fast=True)
 if tokenizer.pad_token is None:
     tokenizer.add_special_tokens({"pad_token": "<|pad|>"})
 
 # Load base mode (4-bit)
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_compute_dtype=torch.float16,
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_quant_type="nf4",
+# )
 
 model = AutoModelForCausalLM.from_pretrained(
-    BASE_MODEL,
-    device_map="auto",
-    quantization_config=bnb_config,
-    torch_dtype=torch.float16,
+    HF_MODEL,
+    # device_map="auto",
+    # quantization_config=bnb_config,
+    # torch_dtype=torch.float16,
+    use_auth_token=HF_TOKEN,
+    torch_dtype=torch.float16
 )
 
-model.resize_token_embeddings(len(tokenizer))
+model.eval()
+
+# model.resize_token_embeddings(len(tokenizer))
 
 # Load LoRA adapter
-model = PeftModel.from_pretrained(model, HF_MODEL, use_auth_token=HF_TOKEN)
-model.eval()
+# model = PeftModel.from_pretrained(model, HF_MODEL, use_auth_token=HF_TOKEN)
+# model.eval()
 
 # Wrap generation in a function
 def generate_quest_from_prompt(user_input: str, max_tokens=150, temperature=0.7, top_p=0.9):
